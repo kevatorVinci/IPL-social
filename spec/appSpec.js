008601b4bdc0509a1a6723b621bcb4b15aa1a106
Kevin-Akator-Vinci <kevin.akator@student.vinci.be>
2025-01-17 16:27:37 +0100

    Creation pakage for tests

---
+++ spec/appSpec.js
@@ -0,0 +1,9 @@
+// Description: This file contains all the test cases for the app.js file.
+
+import { Main } from "../src/app.js";
+
+describe("berlin horloge for minuteSimple", function () {
+ 
+
+
+});
